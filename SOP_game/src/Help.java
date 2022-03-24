af6a873717adcf286f0759c328e3a75d209ddf0d
Kyle <kylechess8@gmail.com>
2022-03-24 12:03:25 -0700

    sync

--- SOP_game/src/Help.java
+++ SOP_game/src/Help.java
@@ -18,12 +18,12 @@ import javax.swing.JTextField;
 public class Help extends JFrame {
 	Help() throws IOException {
 		
-		ImageIcon help = new ImageIcon("help.PNG");
+		ImageIcon help = new ImageIcon("/resources/help.PNG");
 	JLabel image = new JLabel();
 	JPanel helppanel = new JPanel();
 	image.setIcon(help);
 	JLabel not = new JLabel("Input expressions into the textbox on the top of screen. To express NOT use ! or ~");
-	JPanel helptext = new JPanel(); 
+
 	helppanel.add(image);
 helppanel.add(not);
 	add(helppanel);
